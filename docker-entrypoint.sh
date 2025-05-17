a7b422317737ef4c5e3544e5d736f33950e58e27
andrewjamesdawes <andrew@webfx.com>
2025-05-17 14:58:34 -0400

    Add override for host http header

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Set up fallback values for header hosts
+export INTERNAL_HEADER_UP_HOST=${INTERNAL_HEADER_UP_HOST:-${INTERNAL_BACKEND_HOSTNAME}:${INTERNAL_BACKEND_PORT}}
+export EXTERNAL_HEADER_UP_HOST=${EXTERNAL_HEADER_UP_HOST:-${EXTERNAL_BACKEND_HOSTNAME}:${EXTERNAL_BACKEND_PORT}}
+
 # Generate Caddyfile from template
 envsubst </etc/caddy/Caddyfile.template >/etc/caddy/Caddyfile
 
